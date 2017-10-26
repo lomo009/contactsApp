7ce5b0bfb9f9f0ab5192a78b6395f9d9c379a227
logan <logan@dealerspeedleads.com>
2017-10-26 15:07:46 -0500

    publishing to github

---
+++ contacts/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
+
+class App extends Component {
+  state = {
+    contacts: []
+  }
+
+  componentDidMount = () => {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts: contacts })
+    })
+  }
+
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id) 
+    }))
+
+    ContactsAPI.remove(contact)
+  }
+
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <Route exact path='/' render={() => (
+          <ListContacts
+            contacts={this.state.contacts} 
+            onDeleteContact={this.removeContact}
+          />
+        )}/> 
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact= {(contact) => {
+            this.createContact(contact)
+            history.push('/')
+          }}
+          />
+        )}/>
+      </div>
+    );
+  }
+}
+
+export default App;
